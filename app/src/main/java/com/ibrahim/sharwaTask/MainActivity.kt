900456f39324aafe999009281f932ce5a11dbc93
ibrahim <e.ali@tooli.tv>
2022-09-02 05:49:07 +0200

    Initial commit

---
+++ app/src/main/java/com/ibrahim/sharwaTask/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.ibrahim.sharwaTask
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class MainActivity : ComponentActivity() {
+
+    @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
+
+
+
